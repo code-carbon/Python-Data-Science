0167e395e5a3e44022d768c11513e6721ce9754a
codecarbon <71373929+code-carbon@users.noreply.github.com>
2020-09-16 16:59:29 +0530

    Add files via upload

---
+++ data-management.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 12:06:46 2020
+@author: Aishwarya Murali
+"""
+def int_convertor(list):
+    for a in data:
+        data[data.index(a)]=int(a)
+def navigate(order):
+    if(order=="sum"):
+        return sum(data)
+def func_analyser(list):
+    for order in function:
+        navigate(order)
+data=input("--- ").split()
+function=input("+++ ").split()
+int_convertor(data)
+func_analyser(function)
+
